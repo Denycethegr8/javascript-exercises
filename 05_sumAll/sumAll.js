26ba5bfb6212db0fdaf36e54e27b976fc21d5e9b
denyce <jini3209@gmail.com>
2022-04-23 08:09:58 +0900

    solve test including skip

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstN, secondN) {
+    sum = 0;
+    if (typeof firstN !== 'number' || typeof secondN !== 'number' || firstN < 0 || secondN < 0){
+        return 'ERROR';
+    } else if (secondN < firstN){
+        for (let i = secondN; i <= firstN; i++){
+            sum += i;
+        } return sum;
+    } else {
+        for (let j = firstN; j <= secondN; j++){
+            sum += j;
+        } return sum;
+    } 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
