0cb10cd178f109b37f2f352fc8834860e8dc5b9f
denyce <jini3209@gmail.com>
2022-04-22 20:27:32 +0900

    solve all the problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    result = ''
+    if (n < 0) return 'ERROR'
+    for(let counter  = 0; counter < n; counter++){
+        result += str;
+    } return result; 
+}
 
-};
+console.log(repeatString('hey', 10));
+console.log(repeatString('hey', 1));
+console.log(repeatString('hey', 0));
+console.log(repeatString('hey', -1));
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    splitNReverseNJoin = str.split('').reverse().join('');
+    return splitNReverseNJoin;
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
