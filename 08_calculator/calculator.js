81eee1e7acc03bb7795dc4e6d27b668944781a7b
denyce <jini3209@gmail.com>
2022-09-15 21:48:43 +0900

    solve test including skip

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(...arr) {
+  newArr = arr;
+  return newArr[0]+newArr[1];
 };
 
-const subtract = function() {
-	
+const subtract = function(...arr) {
+  newArr = arr;
+	return Math.abs(newArr[0]-newArr[1]);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(length.arr===0) return 0;
+	else return arr.reduce((prev, curr) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(length.arr===0) return 0;
+  else return arr.reduce((prev, curr) => prev * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(...arr) {
+  newArr = arr;
+	return newArr[0]**newArr[1];
 };
 
-const factorial = function() {
-	
+const factorial = function(arr) {
+	if(arr===0 || arr===1) return 1
+  else return arr * factorial(arr-1); 
 };
 
 // Do not edit below this line
