ab9470fbc65b87bc3384f2a033dfebb65ab0638a
denyce <jini3209@gmail.com>
2022-09-21 21:52:52 +0900

    solve test including skip

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.toLowerCase().replace(/[^a-z]/gi, '').replace(/\s/g, '');
+    const reversedStr = newStr.split('').reverse().join('');
+    return newStr === reversedStr;
 };
 
 // Do not edit below this line
