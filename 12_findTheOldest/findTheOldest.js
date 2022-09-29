b59482d38bc1b75f38ec932d63c0259d571a41d0
denyce <jini3209@gmail.com>
2022-09-29 21:38:14 +0900

    solve including skip

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const arr = [];
+    const currentYear = new Date();
+    for(let i=0; i<people.length; i++){
+        if(!people[i].yearOfDeath) people[i].yearOfDeath = currentYear.getFullYear();
+        arr.push(people[i].yearOfDeath - people[i].yearOfBirth);
+        } const i = arr.indexOf(Math.max(...arr))
+    return people[i];
 };
 
 // Do not edit below this line
